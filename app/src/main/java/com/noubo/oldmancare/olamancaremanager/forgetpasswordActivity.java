8cf423d1e7c4bea7e81b546624eb724385ff11cc
wongnoubo <1012084607@qq.com>
2019-02-08 00:27:18 +0800

    找回密码活动设计； 找回密码跳转

--- app/src/main/java/com/noubo/oldmancare/olamancaremanager/forgetpasswordActivity.java
+++ app/src/main/java/com/noubo/oldmancare/olamancaremanager/forgetpasswordActivity.java
@@ -1,19 +1,38 @@
 package com.noubo.oldmancare.olamancaremanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.noubo.oldmancare.R;
 
 public class ForgetpasswordActivity extends AppCompatActivity {
+    public static final String TAG = "ForgetpasswordActivity";
+    private EditText userNameText;
+    private View findpasswordButton;
+    private TextView loginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpassword);
         getSupportActionBar().hide();//隐藏标题栏
-        Log.d("ForgetpasswordActivity",getClass().getSimpleName());
+        Log.d(TAG,getClass().getSimpleName());
 
+        loginText = (TextView)findViewById(R.id.forget_login);
+
+        //登录监听器
+        loginText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent forgetpasswordIntent = new Intent(ForgetpasswordActivity.this,LoginActivity.class);
+                startActivity(forgetpasswordIntent);
+                Log.d(TAG,"跳转登录活动");
+            }
+        });
     }
 }
