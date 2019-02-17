6cff9f88559916b60337de17c421ab14bbb28226
wongnoubo <1012084607@qq.com>
2019-02-17 23:42:43 +0800

    仿造微信的界面设计，逻辑交互暂未处理

--- app/src/main/java/com/noubo/oldmancare/olamancaremanager/MainActivity.java
+++ app/src/main/java/com/noubo/oldmancare/olamancaremanager/MainActivity.java
@@ -7,11 +7,12 @@ import android.util.Log;
 import com.noubo.oldmancare.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("MainActivity", getClass().getSimpleName());
+        Log.d(TAG, getClass().getSimpleName());
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();//隐藏标题栏
     }
 }
