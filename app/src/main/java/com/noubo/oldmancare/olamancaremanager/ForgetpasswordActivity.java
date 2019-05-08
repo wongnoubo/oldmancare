ed7ca0b7cc8863f998af6598ee4c3672f6dc4836
wongnoubo <1012084607@qq.com>
2019-05-09 01:25:26 +0800

    找回密码逻辑简单实现

--- app/src/main/java/com/noubo/oldmancare/olamancaremanager/ForgetpasswordActivity.java
+++ app/src/main/java/com/noubo/oldmancare/olamancaremanager/ForgetpasswordActivity.java
@@ -1,7 +1,9 @@
 package com.noubo.oldmancare.olamancaremanager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.icu.text.LocaleDisplayNames;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -22,6 +24,7 @@ import org.litepal.LitePal;
 import org.litepal.crud.DataSupport;
 import org.litepal.tablemanager.Connector;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ForgetpasswordActivity extends AppCompatActivity {
@@ -30,6 +33,8 @@ public class ForgetpasswordActivity extends AppCompatActivity {
     private View findpasswordButton;
     private TextView loginText;
     private TextView finduserErrorText;
+    private AlertDialog alertDialog;
+    private String myPassWord;
     //用户名正则式校验
     private TextWatcher TextEvent = new TextWatcher() {
         @Override
@@ -62,6 +67,7 @@ public class ForgetpasswordActivity extends AppCompatActivity {
                                 finduserErrorText.setVisibility(View.VISIBLE);
                             }else{
                                 Log.d(TAG,"该账号已被注册");
+                                Log.d(TAG,admins.get(0).getPassword());
                             }
                     }
                 }
@@ -78,7 +84,6 @@ public class ForgetpasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forgetpassword);
         getSupportActionBar().hide();//隐藏标题栏
         Log.d(TAG,getClass().getSimpleName());
-
         loginText = (TextView)findViewById(R.id.forget_login);
         userNameText = (EditText)findViewById(R.id.find_user_name);
         findpasswordButton = findViewById(R.id.find_login_btn);
@@ -105,11 +110,69 @@ public class ForgetpasswordActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 List<Admin> admins = DataSupport.where("username = ?",userNameText.getText().toString()).find(Admin.class);
-                Log.d(TAG,"找回密码："+admins.get(0).getPassword());
-                Intent toLoginIntent = new Intent(ForgetpasswordActivity.this,LoginActivity.class);
-                startActivity(toLoginIntent);
-                Log.d(TAG,"找回密码成功跳转登录");
+                if(!admins.toString().equals("[]")){
+                    myPassWord = admins.get(0).getPassword();
+                    Log.d(TAG,"找回密码："+myPassWord+"，请妥善保管");
+                    initDialog();
+                    showDialog(getWindow().getDecorView());
+                    Log.d(TAG,"找回密码成功跳转登录");
+                }
             }
         });
+    }
+
+    /*
+  初始化AlertDialog
+   */
+    public void initDialog()
+    {
+        //创建AlertDialog的构造器的对象
+        AlertDialog.Builder builder=new AlertDialog.Builder(ForgetpasswordActivity.this);
+        //设置构造器标题
+        builder.setTitle("找回密码");
+        //构造器对应的图标
+        builder.setIcon(R.mipmap.ic_launcher);
+        //构造器内容,为对话框设置文本项(之后还有列表项的例子)
+        builder.setMessage("您的密码是："+myPassWord);
+        //为构造器设置确定按钮,第一个参数为按钮显示的文本信息，第二个参数为点击后的监听事件，用匿名内部类实现
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                //第一个参数dialog是点击的确定按钮所属的Dialog对象,第二个参数which是按钮的标示值
+                finish();//结束当前Activity
+            }
+        });
+        //为构造器设置取消按钮,若点击按钮后不需要做任何操作则直接为第二个参数赋值null
+        builder.setNegativeButton("退出",null);
+        //为构造器设置一个比较中性的按钮，比如忽略、稍后提醒等
+        builder.setNeutralButton("稍后提醒",null);
+        //利用构造器创建AlertDialog的对象,实现实例化
+        alertDialog=builder.create();
+    }
+
+    /*
+    实现Button监听器的除了内部类外的方法
+    点击Button时弹出AlertDialog
+     */
+    public void showDialog(View v)
+    {
+        //当AlertDialog存在实例对象并且没有在展示时
+        if(alertDialog!=null&&!alertDialog.isShowing())
+            alertDialog.show();
+    }
+
+    /**
+     * 表示点击安卓系统返回键时回调的方法
+     */
+    @Override
+    public void onBackPressed()
+    {
+        //当AlertDialog存在实例对象并且没有在展示时
+        if(alertDialog!=null&&!alertDialog.isShowing())
+        {
+            alertDialog.show();
+        }
     }
 }
